1b42b3bff20347d54c42c03d5d39d219e3662c25
kuldeep349 <349kuldeep@gmail.com>
2018-09-29 07:49:33 +0530

    'morningchange';

--- routes/index.js
+++ routes/index.js
@@ -2,28 +2,34 @@
 var express = require('express')
 var app = express()
 
-app.get('/reb', function(req, res) {
-    // render to views/index.ejs template file
-    //res.render('index', {title: 'Application R.E.B'})
-    req.getConnection(function(error, conn) {
-        conn.query('SELECT * FROM classes ORDER BY id DESC',function(err, rows, fields) {
-            if (err) {
-                req.flash('error', err)
-                res.render('index', {
-                    title: 'Class List',
-                    data: ''
-                })
-            } else {
-                res.render('index', {
-                    title: 'Class List',
-                    data: rows
-                })
-            }
-        })
+// app.get('/reb', function(req, res) {
+//     // render to views/index.ejs template file
+//     //res.render('index', {title: 'Application R.E.B'})
+//     req.getConnection(function(error, conn) {
+//         conn.query('SELECT * FROM classes ORDER BY id DESC',function(err, rows, fields) {
+//             if (err) {
+//                 req.flash('error', err)
+//                 res.render('index', {
+//                     title: 'Class List',
+//                     data: ''
+//                 })
+//             } else {
+//                 res.render('index', {
+//                     title: 'Class List',
+//                     data: rows
+//                 })
+//             }
+//         })
+//     })
+//
+// })
+
+app.get('/reb', function(req, res, next) {
+    res.render('index', {
+        title: 'Class List',
+        data: ''
     })
-
 })
-
 app.get('/reb/class-wise', function(req, res, next) {
     res.render('site/class-wise', {
         title: 'Class List',
