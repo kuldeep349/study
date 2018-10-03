e5d9b319d52d2e18893d5742d5ea9327d36890af
kuldeep349 <349kuldeep@gmail.com>
2018-10-03 17:56:02 +0530

    jitendra new links

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var config = {
     database: {
         host:      'localhost',     // database host
         user:       'root',         // your database username
-        password: '',       // your database password
+        password:    '',       // your database password
         port:       '3306',         // default MySQL port
         db:       'reb'         // your database name
     },
