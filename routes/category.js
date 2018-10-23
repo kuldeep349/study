d02494f24b5c531616394cb63998d0653f696bed
kuldeep349 <349kuldeep@gmail.com>
2018-10-23 14:26:29 +0530

    'kush'

--- routes/category.js
+++ routes/category.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var str_replace = require('str_replace');
 session = require('express-session');
+const {database} = require('../db.js')
 app.use(session({
     secret: '2C44-4D44-WppQ38S',
     resave: true,
@@ -22,62 +23,44 @@ var auth = function(req, res, next) {
 app.get('/category_list', auth, function (req, res) {
     res.send("You can only see this after you've logged in.");
 });
-app.get('/categories', auth, function(req, res, next){
-    req.getConnection(function(error, conn) {
-      conn.query('SELECT * FROM tbl_categories ORDER BY id DESC',function(err, rows, fields) {
-          if (err) {
-              req.flash('error', err)
-              res.render('admin/category/category_list', {
-                  title: 'Class List',
-                  data: ''
-              })
-          } else {
-              // render to views/user/list.ejs template file
-              res.render('admin/category/category_list', {
-                  title: 'Class List',
-                  data: rows
-              })
-          }
-      })
-  })
-
+app.get('/categories', auth, async function(req, res, next){
+    
+       var query = 'SELECT * FROM tbl_categories ORDER BY id DESC';
+       results = await database.query(query, [] );
+        res.render('admin/category/category_list', {
+        title: 'Category List',
+        data: JSON.parse(results)
+    })
+          
 })
-app.get('/remove_category/', auth, function(req, res, next){
-    req.getConnection(function(error, conn) {
-    conn.query('delete FROM tbl_categories where id = '+req.query.id,function(err, rows, fields) {
-      if (err) {
-          req.flash('error', err.sqlMessage)
-          res.redirect('../../admin/add-category');
+// TO DELETE CATEGORIES
+app.get('/remove_category/', auth,async function(req, res, next){
+    
+    var query = 'delete FROM tbl_categories where id = '+req.query.id;
+     results = await database.query(query, [] );
+      if (results) {
+        req.flash('success', 'Category Deleted successfully!')
+        res.redirect('/admin/category/add-category');
       } else {
-          req.flash('success', 'Category Deleted successfully!')
-          res.redirect('/admin/add-category');
+        req.flash('error', 'Category Not Deleted!')
+        res.redirect('/admin/category/add-category');
       }
     })
-})
 
-})
-app.get('/add-category', auth, function(req, res, next){
-  req.getConnection(function(error, conn) {
-      conn.query('SELECT * FROM tbl_categories ORDER BY id DESC',function(err, rows, fields) {
-          if (err) {
-              req.flash('error', err)
-              res.render('admin/category/add-category', {
-                  title: 'Class List',
-                  data: ''
-              })
-          } else {
-              // render to views/user/list.ejs template file
-              res.render('admin/category/addcategory', {
-                  title: 'Class List',
-                  data: rows
-              })
-          }
-      })
-  })
 
-})
+// Show ADD CATOGERY FORM
+app.get('/add-category', auth,async function(req, res, next){
 
-app.post('/add-category', auth, function(req, res, next){
+     var query = 'SELECT * FROM tbl_categories ORDER BY id DESC';
+      results = await database.query(query, [] );
+      res.render('admin/category/addcategory', {
+       title: 'Category List',
+       data: JSON.parse(results)
+     })
+          
+})
+// TO ADD CATEGORY
+app.post('/add-category', auth, async function(req, res, next){
   req.assert('category_name','Class Name is required').notEmpty()         //Validate class name
   var errors = req.validationErrors()
   if( !errors ) {
@@ -87,20 +70,23 @@ app.post('/add-category', auth, function(req, res, next){
         slug: str_replace(' ' , '_' , name),
         image:''
       }
-      req.getConnection(function(error, conn) {
-          conn.query('INSERT INTO tbl_categories SET ?', cl, function(err, result) {
-              if (err) {
-                  req.flash('error', err)
-                  res.render('admin/category/add-category', {
-                      title: 'Add New class',
-                      category_name: cl.category_name
-                  })
-              } else {
-                  req.flash('success', 'Category added successfully!')
+    
+          var query = 'INSERT INTO tbl_categories SET ?';
+          results = await database.query(query, [cl] );
+              if (results) {
+                req.flash('success', 'Category added successfully!')
                 res.redirect('/admin/category/add-category');
+                 
+              } else {
+
+                  req.flash('error','Category Addition failed!')
+                  res.redirect('admin/category/add-category')
+                     
+                 
+                
               }
-          })
-      })
+          
+    
   }
   else {   //Display errors to user
       var error_msg = ''
@@ -113,7 +99,7 @@ app.post('/add-category', auth, function(req, res, next){
        * Using req.body.name
        * because req.param('name') is deprecated
        */
-      res.render('classes/addclass', {
+      res.render('/admin/classes/addclass', {
           title: 'Add New Class',
           class_id: req.body.class_id,
           class_name: req.body.class_name,
@@ -123,35 +109,38 @@ app.post('/add-category', auth, function(req, res, next){
 })
 
 /*to get sub categories*/
-// app.get('/get_subcategories/',  function(req, res, next){
-//     req.getConnection(function(error, conn) {
-//     conn.query('select id,name FROM tbl_sub_categories where category_id = '+req.query.id,function(err, rows, fields) {
-//       res.writeHead(200, {'Content-Type': 'application/json'});
-//         res.end(JSON.stringify(rows));
-//     })
-//   })
-// })
-// /*to get nano categories*/
-// app.get('/get_nanocategories/',  function(req, res, next){
-//     req.getConnection(function(error, conn) {
-//     conn.query('select id,name FROM tbl_nano_category where sub_category_id = '+req.query.id,function(err, rows, fields) {
-//       res.writeHead(200, {'Content-Type': 'application/json'});
-//         res.end(JSON.stringify(rows));
-//     })
-//   })
-// })
-// /*to get contents*/
-// app.get('/get_content/',  function(req, res, next){
-//     req.getConnection(function(error, conn) {
+app.get('/get_subcategories',auth,async function(req, res, next){
+    
+    var query = 'select * FROM tbl_sub_categories where category_id = '+req.query.id;
+      //res.writeHead(200, {'Content-Type': 'application/json'});
+      results = await database.query(query, [] );
+      res.send(results);
+    })
+  
+
+/*to get nano categories*/
+app.get('/get_nanocategories',auth,async function(req, res, next){
+    
+    var query = 'select * FROM tbl_nano_category where sub_category_id = '+req.query.id;
+     // res.writeHead(200, {'Content-Type': 'application/json'});
+      results = await database.query(query, [] );
+      res.send(results);
+    })
+  
+
+/*to get contents*/
+// app.get('/get_content',auth,async function(req, res, next){
+  
 //       var query ="SELECT tbl_content.id , tbl_content.type ,tbl_content.title ,tbl_content.description ,tbl_content.created_at , tbl_nano_category.name as nano_category ,tbl_sub_categories.name as sub_category ,tbl_categories.name as category from tbl_content INNER JOIN tbl_nano_category on tbl_content.nano_category_id = tbl_nano_category.id INNER JOIN tbl_sub_categories on tbl_nano_category.sub_category_id = tbl_sub_categories.id INNER JOIN tbl_categories on tbl_sub_categories.category_id = tbl_categories.id";
-//     conn.query(query,function(err, rows, fields) {
-//       res.writeHead(200, {'Content-Type': 'application/json'});
-//         res.end(JSON.stringify(rows));
+   
+//       //res.writeHead(200, {'Content-Type': 'application/json'});
+//       results = await database.query(query, [] );
+//       res.send(results);
 //     })
-//   })
-// })
+ 
+
 /* to add new sub categories*/
-app.post('/sub-category',  function(req, res, next){
+app.post('/sub-category', auth, async  function(req, res, next){
   req.assert('name','Class Name is required').notEmpty()
   req.assert('category_id','category id  is required').notEmpty()     //Validate class name
   var errors = req.validationErrors()
@@ -163,18 +152,20 @@ app.post('/sub-category',  function(req, res, next){
         slug: str_replace(' ' , '_' , name),
         category_id:category_id
       }
-      req.getConnection(function(error, conn) {
-        var q = 'INSERT INTO tbl_sub_categories SET name = "'+cl.name+'" , slug = "'+cl.slug+'" , category_id = "'+cl.category_id+'"';
-          conn.query(q, function(err, result) {
-              if (err) {
-                  req.flash('error', err)
-                    res.redirect('/admin/category/sub_category');
+    
+    var query = 'INSERT INTO tbl_sub_categories SET name = "'+cl.name+'" , slug = "'+cl.slug+'" , category_id = "'+cl.category_id+'"';
+        results = await database.query(query, [cl] );
+              if (results) {
+                req.flash('success', 'Sub Category added successfully!')
+                res.redirect('/admin/category/sub_category');
+                  
               } else {
-                  req.flash('success', 'Sub Category added successfully!')
-                  res.redirect('/admin/category/sub_category');
+                req.flash('error', 'Sub Category Addition Failed!')
+                res.redirect('/admin/category/sub_category');
+                
               }
-          })
-      })
+         
+     
   }
   else {   //Display errors to user
       var error_msg = ''
@@ -187,7 +178,7 @@ app.post('/sub-category',  function(req, res, next){
        * Using req.body.name
        * because req.param('name') is deprecated
        */
-      res.render('classes/addclass', {
+      res.render('/admin/classes/addclass', {
           title: 'Add New Class',
           class_id: req.body.class_id,
           class_name: req.body.class_name,
@@ -196,50 +187,33 @@ app.post('/sub-category',  function(req, res, next){
 
 })
 
-// function to add nano category
-app.get('/sub_category', function(req, res, next){
-  req.getConnection(function(error, conn) {
-      conn.query('SELECT id,name FROM tbl_categories ORDER BY id DESC',function(err, rows, fields) {
-        console.log(err)
-          if (err) {
-              req.flash('error', err)
+// TO SHOW SUB CATEGORY FORM
+app.get('/sub_category',auth, async  function(req, res, next){
+  
+      var query = 'SELECT * FROM tbl_categories ORDER BY id DESC';
+       results = await database.query(query, [] );
+         
               res.render('admin/category/sub_category', {
-                  title: 'Class List',
-                  data: ''
-              })
-          } else {
-              // render to views/user/list.ejs template file
-              res.render('admin/category/sub_category', {
-                  title: 'Class List',
-                  data: rows
-              })
-          }
-      })
-  })
+                  title: 'Sun Category',
+                  data: JSON.parse(results)
+              })           
 })
-app.get('/nano_categories', function(req, res, next){
-    req.getConnection(function(error, conn) {
-        conn.query('SELECT id,name FROM tbl_categories ORDER BY id DESC',function(err, rows, fields) {
-          console.log(err)
-            if (err) {
-                req.flash('error', err)
-                res.render('admin/category/nano_categories', {
-                    title: 'Class List',
-                    data: ''
-                })
-            } else {
+//TO SHOW NANO CATEGORY FORM
+app.get('/nano_categories',auth, async function(req, res, next){
+    
+        var query = 'SELECT * FROM tbl_categories ORDER BY id DESC';
+         results = await database.query(query, [] );
+         
                 // render to views/user/list.ejs template file
                 res.render('admin/category/nano_categories', {
-                    title: 'Class List',
-                    data: rows
+                    title: 'Nano Category',
+                    data: JSON.parse(results)
                 })
-            }
-        })
-    })
+            
   })
 
 /* to add new nano categories*/
-app.post('/add-nano-category',  function(req, res, next){
+app.post('/add-nano-category',auth, async   function(req, res, next){
   req.assert('name','Class Name is required').notEmpty()
   req.assert('sub_category_id','category id  is required').notEmpty()     //Validate class name
   var errors = req.validationErrors()
@@ -251,19 +225,18 @@ app.post('/add-nano-category',  function(req, res, next){
         slug: str_replace(' ' , '_' , name),
         category_id:category_id
       }
-      req.getConnection(function(error, conn) {
-        var q = 'INSERT INTO tbl_nano_category SET name = "'+cl.name+'" , slug = "'+cl.slug+'" , sub_category_id = "'+cl.category_id+'"';
-          conn.query(q, function(err, result) {
-              if (err) {
-                  req.flash('error', err)
-                  console.log(err)
-                    res.redirect('/admin/add_nano_category');
+      
+        var query = 'INSERT INTO tbl_nano_category SET name = "'+cl.name+'" , slug = "'+cl.slug+'" , sub_category_id = "'+cl.category_id+'"';
+         results = await database.query(query, [cl] );
+              if (results) {
+                req.flash('success', 'Nano Category Added Successfully!')
+                  res.redirect('/admin/category/nano_categories');
               } else {
-                  req.flash('success', 'New Category added successfully!')
-                  res.redirect('/admin/add_nano_category');
+                  req.flash('error', 'Nano Category Addtion Failed!')
+                  res.redirect('admin/category/nano_categories');
               }
-          })
-      })
+         
+      
   }
   else {   //Display errors to user
       var error_msg = ''
@@ -284,4 +257,43 @@ app.post('/add-nano-category',  function(req, res, next){
   }
 
 })
+// TO DELETE SUB CATEGORY
+app.get('/delete',async function(req, res, next) {
+    var sbj = { id: req.params.id }
+
+    
+       var query = 'DELETE FROM tbl_sub_categories WHERE id = ' + req.query.id;
+          results = await database.query(query, [sbj] );
+            if ( results) {
+                res.writeHead(200, {'Content-Type': 'application/json'});
+                var obj = {success : 1 , message : 'Sub Category Deleted successfully'}
+                  res.end(JSON.stringify(obj));
+            } else {
+                
+                res.writeHead(200, {'Content-Type': 'application/json'});
+                var obj = {success : 0 , message : 'Sub Category Not Deleted'}
+                  res.end(JSON.stringify(obj));
+            }   
+})
+
+// TO DELETE NANO CATEGORY
+app.get('/nanodelete',async function(req, res, next) {
+    var sbj = { id: req.params.id }
+
+    
+       var query = 'DELETE FROM tbl_nano_category WHERE id = ' + req.query.id;
+          results = await database.query(query, [sbj] );
+            if ( results) {
+                res.writeHead(200, {'Content-Type': 'application/json'});
+                var obj = {success : 1 , message : 'Nano Category Deleted successfully'}
+                  res.end(JSON.stringify(obj));
+            } else {
+                
+                res.writeHead(200, {'Content-Type': 'application/json'});
+                var obj = {success : 0 , message : 'Nano Category Not Deleted'}
+                  res.end(JSON.stringify(obj));
+            }   
+})
+
+
 module.exports = app
